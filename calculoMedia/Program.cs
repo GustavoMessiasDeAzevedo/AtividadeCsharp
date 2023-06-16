5a3e9e9e936b172b57a127e0b32bc48f7dda82ad
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2023-06-16 17:04:49 -0300

    Adicionar arquivos de projeto.

---
+++ calculoMedia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculoMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("|  ::Calcular Média ::  |");
+            Console.WriteLine("=========================");
+
+
+            for (int d = 0; d< 1; d++)
+            {
+                double n, media = 0, media_final;
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write($"Digite um numero {i + 1}: ");
+                    n = Convert.ToDouble(Console.ReadLine());
+                    media += n;
+                };
+                media_final = media;
+                Console.WriteLine("==========");
+                Console.WriteLine($"|   {media_final / 4}   |");
+                Console.WriteLine("==========");
+
+            }
+        }
+    }
+}
